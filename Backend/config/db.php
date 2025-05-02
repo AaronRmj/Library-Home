b7434de13c191986762a76e520cf2b43559abce3
senpai <aaronrmjk@gmail.com>
2025-05-03 00:40:56 +0300

    Connection to the database

---
+++ Backend/config/db.php
@@ -0,0 +1,21 @@
+<?php
+    $host =  "localhost";
+    $dbname = "gestion_livre";
+    $username= "root";
+    $password= "";
+
+try {
+    //connection a mysql
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username,$password);
+    
+    // Activer le mode erreur pour afficher les exceptions
+    $pdo->setAttribute(PDO::ATTR_MODE, PDO:ERRMODE_EXCEPTION);
+
+    echo "connexion reussie"
+}
+
+catch (PDOException $e){
+    die("Erreur de la connexion:" .$e->getMessage());
+}
+
+?>
